3814e1a6d747f0ce770b3593e93e7effc1fd9525
Danae Spentzou <128262908+danaespentz@users.noreply.github.com>
2023-12-28 19:11:50 +0200

    Update query4.py

--- query4.py
+++ query4.py
@@ -35,6 +35,32 @@ sql_str2 = """
     ORDER BY 
         crime_total DESC 
 """
+sql_str3 = """
+    SELECT 
+        YEAR(C.`Date Rptd`) AS year,
+        ROUND(AVG(C.`min_distance`), 4) AS average_distance,
+        COUNT(*) AS crime_total
+    FROM 
+        CrimeDataView4 C
+    WHERE 
+        C.`Weapon Used Cd` LIKE '1%%'
+    GROUP BY
+        C.`Date Rptd`
+    ORDER BY 
+        YEAR(C.`Date Rptd`) ASC 
+"""
+sql_str4 = """
+    SELECT 
+        C.`AREA NAME` AS division,
+        ROUND(AVG(C.`min_distance`), 4) AS average_distance,
+        COUNT(*) AS crime_total
+    FROM 
+        CrimeDataView4 C
+    GROUP BY
+        C.`AREA NAME`
+    ORDER BY 
+        crime_total DESC 
+"""
 
 # Run the query
 start_time = time.time()
@@ -50,6 +76,22 @@ res2 = spark.sql(sql_str2)
 
 # Show the result2
 res2.show()
+print('Total time for SQL: ',time.time() - start_time , 'sec')
+
+# Run the query 3
+start_time = time.time()
+res3 = spark.sql(sql_str3)
+
+# Show the result3
+res3.show()
+print('Total time for SQL: ',time.time() - start_time , 'sec')
+
+# Run the query 4
+start_time = time.time()
+res4 = spark.sql(sql_str4)
+
+# Show the result4
+res4.show()
 print('Total time for SQL: ',time.time() - start_time , 'sec')
 
 # === DataFrame ===
@@ -80,4 +122,32 @@ total_time = time.time() - start_time
 result2.show()
 print('Total time for DataFrame: ', str(total_time), 'sec')
 f.write('Time for Q4ab: ' + str(total_time) + '\n')
+
+start_time = time.time()
+result3 = Crime_Data4.filter(col("Weapon Used Cd").like('1%')) \
+    .groupBy(year("Date Rptd").alias("year")) \
+    .agg({"min_distance": "avg", "*": "count"}) \
+    .orderBy("year", ascending=True) \
+    .withColumnRenamed("avg(min_distance)", "avg_distance") \
+    .withColumnRenamed("count(1)", "crime_total") \
+    .withColumn("avg_distance", round(col("avg_distance"), 4))
+
+total_time = time.time() - start_time
+result3.show()
+print('Total time for DataFrame: ', str(total_time), 'sec')
+f.write('Time for Q4ba: ' + str(total_time) + '\n')
+
+start_time = time.time()
+result4 = Crime_Data4.groupBy("AREA NAME") \
+    .agg({"min_distance": "avg", "*": "count"}) \
+    .orderBy("count(1)", ascending=False) \
+    .withColumnRenamed("avg(min_distance)", "avg_distance") \
+    .withColumnRenamed("count(1)", "crime_total") \
+    .withColumnRenamed("AREA NAME", "division") \
+    .withColumn("avg_distance", round(col("avg_distance"), 4))
+
+total_time = time.time() - start_time
+result4.show()
+print('Total time for DataFrame: ', str(total_time), 'sec')
+f.write('Time for Q4bb: ' + str(total_time) + '\n')
 f.close()
